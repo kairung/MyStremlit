839991c7f25687771ab10692aa23ca32380829c8
kairung <109464720+kairung@users.noreply.github.com>
2023-06-16 14:53:23 +0700

    .commit

--- Home.py
+++ Home.py
@@ -2,4 +2,17 @@
 import streamlit as st
 
 st.header('kairung')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
 
